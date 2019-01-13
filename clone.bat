f50eee6639e0b08161fe3cee0bb46815f8420ea6
liupeng2017 <1131352900@qq.com>
2019-01-13 20:27:30 +0800

    first commit

--- clone.bat
+++ clone.bat
@@ -4,6 +4,7 @@ git add .
 git commit -m "first commit"
 git remote rm origin
 git remote add origin git@github.com:liupeng2017/CreateGif.git
-git pull
+git branch --set-upstream-to=origin/master master
+git pull origin master --allow-unrelated-histories
 git push -u origin master
 pause
