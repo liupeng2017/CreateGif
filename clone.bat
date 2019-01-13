9c4eff93360353abd167b10563abeb259c6f78f6
liupeng2017 <1131352900@qq.com>
2019-01-13 20:24:38 +0800

    first commit

--- clone.bat
+++ clone.bat
@@ -3,6 +3,6 @@ git init
 git add .
 git commit -m "first commit"
 git remote rm origin
-git remote add origin git@github.com:liupeng2017/python.git
+git remote add origin git@github.com:liupeng2017/CreateGif.git
 git push -u origin master
 pause
